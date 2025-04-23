d59e0b215303c1ae84041848f7b84fa368e6f3f1
강현부(SK컴즈) <kanghb@sk.com>
2025-04-23 16:52:25 +0900

    지연 로딩 적용

--- src/main/java/com/crazyatom/beboard/entity/Post.java
+++ src/main/java/com/crazyatom/beboard/entity/Post.java
@@ -6,6 +6,7 @@ import com.crazyatom.beboard.dto.PostRequestDto;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
